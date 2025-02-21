64fd8ac28aedd4639a45ffee1168b99e3b489da7
Asxtrial <145523832+Asxtrial@users.noreply.github.com>
2025-02-21 14:35:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.querySelector(".loader-wrapper").style.display = "none";
+        document.querySelectorAll(".section").forEach(element => {
+            element.classList.add("show");
+        });
+    }, 1000);
+});
+
+let currentSection = 0;
+const sections = document.querySelectorAll(".section");
+
+document.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    if (event.deltaY > 0 && currentSection < sections.length - 1) {
+        currentSection++;
+    } else if (event.deltaY < 0 && currentSection > 0) {
+        currentSection--;
+    }
+    sections[currentSection].scrollIntoView({ behavior: "smooth" });
+}, { passive: false });
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
